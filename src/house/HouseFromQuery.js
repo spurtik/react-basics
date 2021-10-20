9693a3dd4b2f82b85d9c24c93dc3aa835b32eea5
Spurti <spurtik@users.noreply.github.com>
2021-10-20 15:47:41 +1100

    react simple app

---
+++ src/house/HouseFromQuery.js
@@ -0,0 +1,15 @@
+import { useParams } from "react-router";
+import House from "./";
+import { useContext } from "react";
+import HousesContext from "../context/housesContext";
+
+const HouseFromQuery = () => {
+    const { id } = useParams();
+    const allHouses = useContext(HousesContext);
+    
+    const house = allHouses.find((h) => h.id === parseInt(id));
+    if(!house) return <div>House not found.</div>;
+    return <House house={house} />;
+}
+ 
+export default HouseFromQuery;
